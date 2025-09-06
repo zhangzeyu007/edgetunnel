bf81436540da1cf472f243c33e42d27389c789c4
Jobs-Devin <30886537+zhangzeyu007@users.noreply.github.com>
2025-09-06 12:20:11 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = '8d29f3f9-fb41-4eb3-b042-09b2d782a230';
 let proxyIP = '';
 let DNS64Server = '';
 //let sub = '';
@@ -4568,3 +4568,4 @@ async function nginx() {
 	`
     return text;
 }
+
